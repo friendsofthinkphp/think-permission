f96766dd4d7416e7a6825a4909a28c6b15f4e0b7
xiaodit <saybabi@163.com>
2019-11-08 22:48:30 +0800

    update

---
+++ src/Config/config.php
@@ -0,0 +1,36 @@
+<?php
+
+use xiaodi\Permission\Model\User;
+use xiaodi\Permission\Model\Permission;
+use xiaodi\Permission\Model\Role;
+use xiaodi\Permission\Model\PermissionRoleAccess;
+use xiaodi\Permission\Model\UserRoleAccess;
+
+return [
+    'auth_super_id' => 1,
+
+    'user' => [
+        'table' => 'auth_user',
+        'model' => User::class
+    ],
+
+    'permission' => [
+        'table' => 'auth_permission',
+        'model' => Permission::class
+    ],
+
+    'role' => [
+        'table' => 'auth_role',
+        'model' => Role::class
+    ],
+
+    'role_permission_access' => [
+        'table' => 'auth_role_permission_access',
+        'model' => PermissionRoleAccess::class
+    ],
+
+    'user_role_access' => [
+        'table' => 'auth_user_role_access',
+        'model' => UserRoleAccess::class
+    ],
+];
