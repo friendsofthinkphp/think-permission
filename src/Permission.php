d913044f27ab5b74e451dd2b58410232bc80990f
xiaodi <liangjinbiao@live.com>
2019-12-04 13:18:52 +0800

    update

--- src/Permission.php
+++ src/Permission.php
@@ -3,8 +3,6 @@
 namespace xiaodi\Permission;
 
 use think\App;
-use xiaodi\Permission\Contract\PermissionHandleContract;
-use xiaodi\Permission\Contract\UserContract;
 
 class Permission
 {
@@ -16,34 +14,5 @@ class Permission
     {
         $this->app = $app;
         $this->config = config('permission');
-    }
-
-    public function user()
-    {
-        if (!$this->app->user) {
-            throw new \Exception('没有注入用户模型');
-        }
-
-        if (false === $this->app->user instanceof UserContract) {
-            throw new \Exception('not implement xiaodi\Permission\Contract\UserContract');
-        }
-
-        return $this->app->user;
-    }
-
-    public function handleMiddleware()
-    {
-        $className = $this->config['middleware'];
-
-        if (!$className) {
-            throw new \Exception('没有配置权限中间件');
-        }
-
-        $handle = new $className();
-        if (false === $handle instanceof PermissionHandleContract) {
-            throw new \Exception('not implements \xiaodi\Permission\Contract\PermissionHandleContract');
-        }
-
-        return $handle;
     }
 }
