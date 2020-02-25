54ab1c44610c0d3a28edcd417ade7dfe72c6b64a
Eden Leung <31346973+edenleung@users.noreply.github.com>
2020-02-25 19:18:37 +0800

    Update PermissionService.php

--- src/PermissionService.php
+++ src/PermissionService.php
@@ -3,12 +3,15 @@
 namespace xiaodi\Permission;
 
 use xiaodi\Permission\Middleware\Permission;
+use xiaodi\Permission\Middleware\Role;
 
 class PermissionService extends \think\Service
 {
     public function register()
     {
         $this->app->bind('auth', Permission::class);
+        $this->app->bind('auth.permission', Permission::class);
+        $this->app->bind('auth.role', Role::class);
     }
 
     public function boot()
